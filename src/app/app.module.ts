14e64f216f9bd2ec804725fafe6f4b6aafe080a7
Óscar Llorente <llorente_oscar@hotmail.com>
2020-01-16 22:20:33 +0000

    Componentes y Rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
+import { RouterModule, Routes } from '@angular/router';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { CuerpodosComponent } from './cuerpodos/cuerpodos.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+const routes: Routes = [
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: '', component: InicioComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    FooterComponent
+    FooterComponent,
+    ContactoComponent,
+    CuerpoComponent,
+    CuerpodosComponent,
+    NosotrosComponent,
+    InicioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
